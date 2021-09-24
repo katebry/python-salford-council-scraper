b55616ddfa7c583168b21a615878996357a5f96f
Kate Bryan <katebryan31@gmail.com>
2021-09-24 13:52:52 +0100

    feat: updated scraper

--- salfordcouncilwebscraper.py
+++ salfordcouncilwebscraper.py
@@ -1,8 +1,33 @@
-from urllib.request import urlopen
+import mechanicalsoup
+import re
+from bs4 import BeautifulSoup as BS
+
+browser = mechanicalsoup.StatefulBrowser()
+
+customerPostCode = ""
+customerAddress = ""
 
 url = "https://www.salford.gov.uk/bins-and-recycling/bin-collection-days/"
 
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-print(html)
+page = browser.open(url)
+
+browser.select_form('form[action="#propertylist"]')
+browser["prop"] = customerPostCode
+browser.form.print_summary()
+browser.submit_selected()
+
+propertySearchResults = browser.page.find_all(
+    "a")
+
+for propertySearchResult in propertySearchResults:
+    if propertySearchResult.find(text=re.compile(customerAddress)):
+        addressLink = propertySearchResult
+        break
+
+print(addressLink)
+
+browser.follow_link(addressLink)
+
+binDataPage = browser.page.find_all('div', attrs={'class': 'waste'})
+
+print(binDataPage)
